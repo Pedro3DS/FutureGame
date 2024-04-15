d10803dc6e054f88f101fb5b4a8341b66dcee4d9
Pedro3DS <pedroca2787@gmail.com>
2024-04-15 14:58:09 -0400

    update
    foi feito o script das plataformas que se movimentam.

--- Assets/Scipts/playerMovement.cs
+++ Assets/Scipts/playerMovement.cs
@@ -2,57 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
-    public float speed = 5f;
-    public float jumpForce = 10f;
-    //private bool isGround;
-    //public Transform checkGround;
-    //public LayerMask layerGround;
-
-    private Rigidbody2D rb2d;
-    private Animator animator;
-
-    //private bool isAttacking = false;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-
-    private void FixedUpdate()
-    {
-        Movement();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    void Movement()
-    {
-        float movimentoHorizontal = Input.GetAxis("Horizontal");
-        rb2d.velocity = new Vector2(movimentoHorizontal * speed, rb2d.velocity.y);
-
-        if (movimentoHorizontal < 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (movimentoHorizontal > 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-
-        if (movimentoHorizontal != 0)
-        {
-            animator.SetBool("Run", true);
-        }
-        else
-        {
-            animator.SetBool("Run", false);
-        }
     }
 }
