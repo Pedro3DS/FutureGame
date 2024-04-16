5127d036e3a2f322d31457faf0da46f408654aae
Pedro3DS <pedroca2787@gmail.com>
2024-04-16 16:05:34 -0400

    update
    foi feita a mecânica de engrenagem

--- Assets/Scipts/AudioManager.cs
+++ Assets/Scipts/AudioManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] public AudioSource musicSource;
+    [SerializeField] public AudioSource sfxSource;
 
     [SerializeField] public AudioClip backgroundMusic;
+    [SerializeField] public AudioClip gear;
 
     void Start()
     {
         musicSource.clip = backgroundMusic;
         musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
     }
 }
