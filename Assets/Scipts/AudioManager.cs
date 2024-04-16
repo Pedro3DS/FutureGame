855dc5cc434900cb585e560b464729dd66c464b8
Mauriiicio <mauricio1desouza@gmail.com>
2024-04-16 14:53:26 -0400

    Revert "Merge branch 'main' of https://github.com/Pedro3DS/FutureGame into feature/tiroPlayer"
    This reverts commit ea2057d5092bb136082c3ca502c05fdcb5d70ec1, reversing
    changes made to e6bc2c6c47b03bd143504497f3feed7e64d793af.

--- Assets/Scipts/AudioManager.cs
+++ Assets/Scipts/AudioManager.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] public AudioSource musicSource;
-    [SerializeField] public AudioSource sfxSource;
 
     [SerializeField] public AudioClip backgroundMusic;
-    [SerializeField] public AudioClip getGearSound;
 
     void Start()
     {
         musicSource.clip = backgroundMusic;
         musicSource.Play();
-    }
-
-    public void PlaySfx(AudioClip clip)
-    {
-        sfxSource.PlayOneShot(clip);
     }
 }
